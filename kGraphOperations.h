b3a23b277305a8275bd3b915af08b1a5c9524653
dRyaba <danil_rw@mail.ru>
2023-11-26 18:36:55 +0700

    working version

--- kGraphOperations.h
+++ kGraphOperations.h
@@ -4,10 +4,10 @@
 #define GRADUATE_WORK_KGRAPHOPERATIONS_H
 
 struct Graph {
-    std::vector<unsigned short> KAO, FO;
+    std::vector<int> KAO, FO;
     std::vector<double> PArray;
     Graph() = default;
-    Graph(std::vector<unsigned short> KAO, std::vector<unsigned short> FO, std::vector<double> PArray) {
+    Graph(std::vector<int> KAO, std::vector<int> FO, std::vector<double> PArray) {
         this->KAO = std::move(KAO);
         this->FO = std::move(FO);
         this->PArray = std::move(PArray);
@@ -16,12 +16,12 @@ struct Graph {
 };
 
 struct kGraph: public Graph {
-    std::vector<unsigned short> Targets;
+    std::vector<int> Targets;
 
     kGraph() = default;
 
-    kGraph(std::vector<unsigned short> KAO, std::vector<unsigned short> FO, std::vector<double> PArray,
-           std::vector<unsigned short> Targets) : Graph() {
+    kGraph(std::vector<int> KAO, std::vector<int> FO, std::vector<double> PArray,
+           std::vector<int> Targets) : Graph() {
         this->KAO = std::move(KAO);
         this->FO = std::move(FO);
         this->PArray = std::move(PArray);
@@ -32,20 +32,20 @@ struct kGraph: public Graph {
 
 
 
-kGraph InducedKGraph(kGraph &G, std::vector<unsigned short> spisok, unsigned short Number);
+kGraph InducedKGraph(const kGraph &G, std::vector<int> spisok, int Number);
 
-int SearchEdge(const kGraph &G, int i, int j) {
+int SearchEdge(const kGraph &G, const int i,const int j) {
 //вычисляет номер ребра из i в j в массиве FO
     int res = G.FO.size();
-    for (int k = G.KAO[i - 1]; k <= G.KAO[i] - 1; k++)
+    for (int k = G.KAO[i - 1]; k < G.KAO[i]; k++)
         if (G.FO[k] == j)
             res = k;
     return res;
 }
 
-kGraph DeleteEdgeK(const kGraph& G, unsigned short u, unsigned short v) {
+kGraph DeleteEdgeK(const kGraph& G,const int u,const int v) {
 //    Удаляет из графа ребро(u, v)
-    unsigned short i, e, f;
+    int i, e, f;
     e = SearchEdge(G, u, v);
     f = SearchEdge(G, v, u);
     kGraph Result;
@@ -55,19 +55,19 @@ kGraph DeleteEdgeK(const kGraph& G, unsigned short u, unsigned short v) {
         Result.FO.resize(G.FO.size() - 2);
         Result.PArray.resize(G.PArray.size() - 2);
         if (e < f) {
-            for (i = 0; i <= u - 1; i++)
+            for (i = 0; i < u; i++)
                 Result.KAO[i] = G.KAO[i];
-            for (i = u; i <= v - 1; i++)
+            for (i = u; i < v; i++)
                 Result.KAO[i] = G.KAO[i] - 1;
-            for (i = v; i <= G.KAO.size() - 1; i++)
+            for (i = v; i < G.KAO.size(); i++)
                 Result.KAO[i] = G.KAO[i] - 2;
             if (e > 0)
-                for (i = 0; i <= e - 1; i++) {
+                for (i = 0; i < e; i++) {
                     Result.FO[i] = G.FO[i];
                     Result.PArray[i] = G.PArray[i];
                 }
             if (f > e + 1)
-                for (i = e; i <= f - 2; i++) {
+                for (i = e; i < f - 1; i++) {
                     Result.FO[i] = G.FO[i + 1];
                     Result.PArray[i] = G.PArray[i + 1];
                 }
@@ -77,50 +77,51 @@ kGraph DeleteEdgeK(const kGraph& G, unsigned short u, unsigned short v) {
                     Result.PArray[i] = G.PArray[i + 2];
                 }
         } else {
-            for (i = 0; i <= v - 1; i++)
+            for (i = 0; i < v; i++)
                 Result.KAO[i] =G.KAO[i];
-            for (i = v; i <= u - 1; i++)
+            for (i = v; i < u; i++)
                 Result.KAO[i]=G.KAO[i] - 1;
-            for (i = u; i <= G.KAO.size() - 1; i++)
+            for (i = u; i < G.KAO.size(); i++)
                 Result.KAO[i] = G.KAO[i] - 2;
             if (f > 0)
-                for (i = 0; i <= f - 1; i++) {
+                for (i = 0; i < f; i++) {
                     Result.FO[i] = G.FO[i];
                     Result.PArray[i] = G.PArray[i];
                 }
             if (e > f + 1)
-                for (i = f; i <= e - 2; i++) {
+                for (i = f; i < e - 1; i++) {
                     Result.FO[i] = G.FO[i + 1];
                     Result.PArray[i] = G.PArray[i + 1];
                 }
             if (G.FO.size() > e + 1)
-                for (i = e - 1; i <= G.FO.size() - 3; i++) {
+                for (i = e - 1; i < G.FO.size() - 2; i++) {
                     Result.FO[i] = G.FO[i + 2];
                     Result.PArray[i] = G.PArray[i + 2];
                 }
         }
     }
     Result.Targets.resize(G.Targets.size());
-    for (i = 0; i <= Result.Targets.size() - 1; i++)
+    for (i = 0; i < Result.Targets.size(); i++)
         Result.Targets[i] = G.Targets[i];
     return Result;
 }
 
-bool KComponent(kGraph G) {
+bool KComponent(kGraph &G) {
 //{Выделяет компоненту связности графа G, содержащую все целевые вершины.
 //Если это возможно, то компонента сохраняется как G, а результат - истина.
 //В противном случае граф не изменяется, а результат - ложь.
 //Переформирование графа G происходит лишь в том случае, когда он несвязен.
 //Если граф не содержит целевых вершин, то результат - ложь.
-//Преименение функции к пустому графу некорректно}
-    int i, j, sum, l, sum1, SumAll;
-    bool Result;
-    std::vector<unsigned short> A, B, Spot;
-    sum1 = 0;
-    for (i = 1; i < G.Targets.size() - 1; i++)
+//Применение функции к пустому графу некорректно}
+    int sum1 = 0;
+    for (int i = 1; i < G.Targets.size(); i++)
         if (G.Targets[i] == 1)
             sum1++;
     if (sum1 == 1) {
+        // как ищется компонента связности?
+        // идем по массиву Таргетс со 2го до последнего элемента
+        // и если там есть всего 1 целевая вершина, то возвращаем безвершинный граф
+        // а если первый элемент таргетс 1
         G.KAO.resize(2);
         G.KAO[0] = 0;
         G.KAO[1] = 0;
@@ -129,25 +130,26 @@ bool KComponent(kGraph G) {
         G.Targets.resize(2);
         G.Targets[0] = 0;
         G.Targets[1] = 0;
-        Result = true;
+        return true;
     } else {
-        sum = 1;
-        SumAll = 1;
+        std::vector<int> A, B, Spot;
+        int sum = 1;
+        int SumAll = 1;
         A.resize(1);
-        for (i = 1; i <= G.Targets.size() - 1; i++)
+        for (int i = 1; i < G.Targets.size(); i++)
             if (G.Targets[i] == 1) {
                 A[0] = i;
                 break;
             }
-        l = A.size();
+        int l = A.size();
         Spot.resize(G.KAO.size());
-        for (i = 1; i < G.KAO.size() - 1; i++)
+        for (int i = 1; i < G.KAO.size(); i++)
             Spot[i] = 2;
         Spot[A[0]] = 1;
         while (l > 0) {
             B.resize(0);
-            for (i = 0; i < A.size() - 1; i++)
-                for (j = G.KAO[A[i] - 1]; j < G.KAO[A[i] - 1]; j++)
+            for (int i : A)
+                for (int j = G.KAO[i - 1]; j < G.KAO[i]; j++)
                     if (Spot[G.FO[j]] == 2) {
                         B.resize(B.size() + 1);
                         B[B.size() - 1] = G.FO[j];
@@ -160,50 +162,46 @@ bool KComponent(kGraph G) {
             A.resize(B.size());
             l = A.size();
             if (l > 0)
-                for (i = 0; i <= l - 1; i++)
+                for (int i = 0; i < l; i++)
                     A[i] = B[i];
         }
-        if (sum == sum1)
-            Result = true;
-        else
-            Result = false;
+        bool Result = (sum == sum1);
         if (Result && (SumAll < (G.KAO.size() - 1)))
             G = InducedKGraph(G, Spot, 1);
+        return Result;
     }
-    return Result;
 }
 
-bool Boolka(kGraph &G, std::vector<unsigned short> spisok, unsigned short Number, unsigned short i, unsigned short j) {
+bool Boolka(const kGraph &G, std::vector<int> spisok, int Number, int i, int j) {
     if (Number == 1)
         return true;
-    else if ((spisok[i] == 0) && (spisok[G.FO[j]] == 0))
-        return false;
-    else
-        return true;
+//    if (!(spisok[i] || spisok[G.FO[j]]))
+//        return false;
+//    return true;
+    return spisok[i] || spisok[G.FO[j]];
 }
 
-kGraph InducedKGraph(kGraph &G, std::vector<unsigned short> spisok, unsigned short Number) {
+kGraph InducedKGraph(const kGraph &G, std::vector<int> spisok, int Number) {
 //{Восстанавливает подграф K-графа G, в который входят  ребра с номером Number в списке spisok}
-    std::vector<unsigned short> S, FO, KAO, Targets;
-    unsigned short i, j, l;
+    std::vector<int> S, FO, KAO, Targets;
     std::vector<double> PArray;
     S.resize(spisok.size());
-    j = 0;
-    for (i = 1; i <= spisok.size() - 1; i++)
-        if ((spisok[i] = Number) || (spisok[i] = 0)) {
-            j++;
-            S[i] = j;
+    int k = 0;
+    for (int i = 1; i < spisok.size(); i++)
+        if ((spisok[i] == Number) || (spisok[i] == 0)) {
+            k++;
+            S[i] = k;
         } else
             S[i] = 0;
-    KAO.resize(j + 1);
+    KAO.resize(k + 1);
     KAO[0] = 0;
     FO.resize(G.FO.size());
     PArray.resize(G.FO.size());
-    l = 0;
-    for (i = 1; i <= spisok.size() - 1; i++)
+    int l = 0;
+    for (int i = 1; i < spisok.size(); i++)
         if (S[i] != 0) {
             KAO[S[i]] = KAO[S[i] - 1];
-            for (j = G.KAO[i - 1]; j <= G.KAO[i - 1]; j++)
+            for (int j = G.KAO[i - 1]; j < G.KAO[i]; j++)
                 if ((S[G.FO[j]] != 0) && Boolka(G, spisok, Number, i, j)) {
                     KAO[S[i]]++;
                     FO[l] = S[G.FO[j]];
@@ -216,7 +214,7 @@ kGraph InducedKGraph(kGraph &G, std::vector<unsigned short> spisok, unsigned sho
     Targets.resize(KAO.size());
     Targets[0] = 0;
     kGraph Result(KAO, FO, PArray, Targets);
-    for (i = 1; G.Targets.size() - 1; i++)
+    for (int i = 1; i < G.Targets.size(); i++)
         if (S[i] > 0)
             Result.Targets[S[i]] = G.Targets[i];
 
--- main.cpp
+++ main.cpp
@@ -1,171 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <iomanip>
 #include "kGraphOperations.h"
-#include "Matrix.h"
-
-//проверить быстрее ли вычисление на статических массивах(мб ещё на calloc, malloc)
-
-Matrix::Matrix() {
-    this->rows = 0;
-    this->columns = 0;
-    this->values = nullptr;
-}
-
-Matrix::Matrix(int rows, int columns) {
-    this->values = new int* [rows];
-    for (int i = 0; i < rows; i++) {
-        this->values[i] = new int[columns];
-        for (int j = 0; j < columns; ++j)
-            this->values[i][j] = 0;
-        this->values[i][i] = 1;
-    }
-}
-
-Matrix::Matrix(int rows, int columns, int value) : rows(rows), columns(columns) {
-    this->values = new int* [rows];
-    for (int i = 0; i < rows; i++) {
-        this->values[i] = new int[columns];
-        for (int j = 0; j < columns; ++j)
-            this->values[i][j] = 0;
-        this->values[i][i] = value;
-    }
-}
-
-Matrix::Matrix(int rows, int columns, int *values) : rows(rows), columns(columns) {
-    this->values = new int *[rows];
-    for (int i = 0; i < rows; i++) {
-        this->values[i] = new int[columns];
-        for (int j = 0; j < columns; ++j)
-            this->values[i][j] = 0;
-        this->values[i][i] = values[i];
-    }
-}
-
-Matrix::Matrix(int rows, int columns, int **values) : rows(rows), columns(columns) {
-    this->values = values;
-}
-
-// Получение минора
-Matrix Matrix::getMinor(int a, int b) {
-    int **newValues = new int *[rows - 1];
-    int ni = 0, nj = 0;
-    for (int i = 0; i < rows; ++i) {
-        if (i == a - 1)
-            continue;
-        newValues[ni] = new int[columns - 1];
-        for (int j = 0; j < columns; ++j) {
-            if (j == b - 1)
-                continue;
-            newValues[ni][nj++] = values[i][j];
-        }
-        ni++;
-        nj = 0;
-    }
-    return {rows - 1, columns - 1, newValues};
-}
-
-// Сложение матриц
-Matrix Matrix::operator+(const Matrix &a) const {
-    if ((rows != a.rows) && (columns != a.columns)) {
-        std::cout << "Ошибка: размеры матриц не совпадают.";
-        exit(1);
-    }
-    int **newValues = new int *[rows];
-    for (int i = 0; i < rows; i++) {
-        newValues[i] = new int[columns];
-        for (int j = 0; j < columns; j++)
-            newValues[i][j] = this->values[i][j] + a.values[i][j];
-    }
-    return {rows, columns, newValues};
-}
-
-// Сравнение матриц
-bool Matrix::operator==(const Matrix &b) {
-    if ((rows != b.rows) && (columns != b.columns))
-        return false;
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-            if (this->values[i][j] != b.values[i][j])
-                return false;
-    return true;
-}
-
-// Вывод матрицы на экран
-void Matrix::operator+() {
-    for (int i = 0; i < this->rows; ++i) {
-        for (int j = 0; j < this->columns; ++j) {
-            printf("%d ", this->values[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// Умножение матриц
-Matrix Matrix::operator*(const Matrix &b) const {
-    if (columns != b.rows) {
-        std::cout << "Ошибка: количество столбцов первой матрицы не совпадает с количеством строк второй матрицы.";
-        exit(1);
-    }
-    int **newValues = new int *[rows];
-    for (int i = 0; i < rows; i++) {
-        newValues[i] = new int[b.columns];
-        for (int j = 0; j < b.columns; j++) {
-            int tempSum = 0;
-            for (int k = 0; k < columns; ++k) {
-                tempSum += this->values[i][k] * b.values[k][j];
-            }
-            newValues[i][j] = tempSum;
-        }
-    }
-    return {rows, b.columns, newValues};
-}
-
-// Взятие строки
-int *Matrix::operator[](int num) {
-    return values[num];
-}
-
-
-// Транспонирование матрицы
-Matrix Matrix::operator~() {
-    int **newValues = new int *[rows];
-    for (int i = 0; i < rows; ++i) {
-        newValues[i] = new int[columns];
-        for (int j = 0; j < columns; ++j)
-            newValues[i][j] = this->values[j][i];
-    }
-    return {columns, rows, newValues};
-}
-
-Matrix::~Matrix() {
-    for (int i = 0; i < columns; ++i)
-        delete[] values[i];
-    delete[] values;
-}
 
 
 bool CheckEdge(kGraph &G, int i, int j);
 
-bool CheckDistance(kGraph &G, unsigned short d, unsigned short Nconst);
+bool CheckDistance(kGraph &G, int d, int Nconst);
 
-double Factoring(kGraph &G, unsigned short variant, int NumberOfRec, unsigned short d, unsigned short Nconst);
+double Factoring(kGraph &G, int variant, int NumberOfRec, int d, int Nconst);
 
-double ReliabilityDiamConstr(kGraph &G, unsigned short d) {
+double ReliabilityDiamConstr(kGraph &G, int d) {
 //{Расчет вероятности связности с ограничением на диаметр d методом ветвления.
 // Используется выделение комп-ты связности с целевыми в-ми, разделение ветвей,
 // встроенная ф-я проверки на расстояния}
-    unsigned short Nconst;
-    Nconst = (G.KAO.size()) * (G.KAO.size());
+    int Nconst = G.KAO.size() * G.KAO.size();
     double t = Factoring(G, 0, 0, d, Nconst);
     return t;
 }
 
-//Зачем NumberofRec? чтобы знать какой шаг ветвеления?
-double Factoring(kGraph &G, unsigned short variant, int NumberOfRec, unsigned short d, unsigned short Nconst) {
+double Factoring(kGraph &G, int variant, int NumberOfRec, int d, int Nconst) {
 //Ветвление, variant=0 - после удаления, variant=1 - после обнадеживания ребра
-    double p;
     int i, j, k;
-    int numberOfRec = NumberOfRec + 1;
+    NumberOfRec++;
+    //правильно ли тут расставлены фигурные скобки
     if (variant == 0) {
         if (!KComponent(G))
             return 0;
@@ -181,79 +40,109 @@ double Factoring(kGraph &G, unsigned short variant, int NumberOfRec, unsigned sh
     if (i == G.FO.size())
         return 1;
     else {
-        p = G.PArray[i];
+        double p = G.PArray[i];
         G.PArray[i] = 1;
-        for (j = 1; j <= G.KAO.size() - 1; j++)
+        for (j = 1; j < G.KAO.size(); j++)
             if (G.KAO[j] > i)
                 break;
-//        int g = G.KAO[G.FO[i] - 1];
-//        int g_top = G.KAO[G.FO[i]] - 1;
-        for (k = G.KAO[G.FO[i] - 1]; k <= G.KAO[G.FO[i]] - 1; k++)
+        for (k = G.KAO[G.FO[i] - 1]; k < G.KAO[G.FO[i]]; k++)
             if (G.FO[k] == j)
                 break;
         G.PArray[k] = 1;
         kGraph T = DeleteEdgeK(G, G.FO[i], j);
-        double a1 = p * Factoring(G, 1, numberOfRec, d, Nconst), a2 = (1 - p) * Factoring(T, 0, numberOfRec, d, Nconst);
-        if (a1 + a2 != 1) {
-            std::cout <<"a";
-        }
-        return a1 + a2;
+        double branch1 = p * Factoring(G, 1, NumberOfRec, d, Nconst);
+        double branch2 = (1 - p) * Factoring(T, 0, NumberOfRec, d, Nconst);
+
+        return branch1 + branch2;
     }
 }
 
-bool CheckDistance(kGraph &G, unsigned short d, unsigned short Nconst) {
+bool CheckDistance(kGraph &G, int d, int Nconst) {
 //Методом Флойда строит матрицу расстояний и проверяет нужные
-    unsigned short N = G.KAO.size() - 1;
-    Matrix M(N + 1, N + 1);
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
+    int N = G.KAO.size();
+    std::vector<std::vector<int> > M(N);
+
+    for (int i = 0; i < N; i++) {
+        M[i].resize(N + 1);
+    }
+    for (int i = 1; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
             if (CheckEdge(G, i, j))
                 M[i][j] = 1;
             else M[i][j] = Nconst;
         }
         for (int k = 1; k < N; k++) {
-            for (i = 1; i <= k - 1; i++) {
-                for (int j = i + 1; j <= k - 1; j++)
+            for (i = 1; i < k; i++) {
+                for (int j = i + 1; j < k; j++)
                     if (M[i][j] > M[i][k] + M[j][k])
                         M[i][j] = M[i][k] + M[j][k];
-
-                for (int j = k + 1; j <= N; j++)
+                for (int j = k + 1; j < N; j++)
                     if (M[i][j] > M[i][k] + M[k][j])
                         M[i][j] = M[i][k] + M[k][j];
             }
-            for (i = k + 1; i <= N; i++) {
-                for (int j = i + 1; j <= N; j++)
+            for (i = k + 1; i < N; i++) {
+                for (int j = i + 1; j < N; j++)
                     if (M[i][j] > M[k][i] + M[k][j])
                         M[i][j] = M[k][i] + M[k][j];
             }
         }
     }
-    bool Result = true;
-    for (int i = 1; i <= N - 1; i++)
+    for (int i = 1; i < N - 1; i++)
         if (G.Targets[i] == 1)
-            for (int j = i + 1; j < N; j++)
+            for (int j = i + 1; j < N - 1; j++)
                 if ((G.Targets[j] == 1) && (M[i][j] > d))
-                    Result = false;
-    return Result;
+                    return false;
+    return true;
 }
 
-bool CheckEdge(kGraph& G, int i, int j) {
+bool CheckEdge(kGraph &G, int i, int j) {
 //вычисляет номер ребра из i в j в массиве FO
-    bool Result = false;
-    for (int k = G.KAO[i - 1]; k < G.KAO[i] - 1; k++)
+//ДР: скорее не вычисляет, а проверяет наличие
+    for (int k = G.KAO[i - 1]; k < G.KAO[i]; k++)
         if (G.FO[k] == j)
-            Result = true;
-    return Result;
+            return true;
+    return false;
 }
 
 int main() {
-    std::vector<unsigned short> KAO, FO, Targets;
+    std::ifstream fin("input.txt");
+    if (!fin) {
+        std::cout << "Error!" << std::endl;
+        return 2;
+    }
+    std::vector<int> KAO, FO, Targets;
+
+    std::string line;
+    std::getline(fin, line,'\n');
+    std::stringstream skao(line);
+    std::string temp;
+    while (getline(skao, temp, ','))
+        KAO.push_back(std::stoi(temp));
+
+    std::getline(fin, line,'\n');
+    std::stringstream sfo(line);
+    while (getline(sfo, temp, ','))
+        FO.push_back(std::stoi(temp));
+
+    std::getline(fin, line,'\n');
+    std::stringstream stargets(line);
+    while (getline(stargets, temp, ','))
+        Targets.push_back(std::stoi(temp));
+    std::getline(fin, line,'\n');
+    fin.close();
+    size_t found = line.find(',');
+    if (found!=std::string::npos)
+        line[found] = '.';
+    double p = std::stod(line);
     int d = 4;
-    KAO = {0,1,3,4};
-    FO = {2,1,3,2};
-    std::vector<double> Parray(FO.size(), 0.9);
-    Targets = {1,0,1};
-//    Parray = {0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9};
-    kGraph G(KAO,FO,Parray, Targets);
-    std::cout<< ReliabilityDiamConstr(G,d);
+//    double p = 0.9;
+//    KAO = {0, 4, 8, 12, 16, 20};
+//    FO = {2, 3, 4, 5, 1, 3, 4, 5, 1, 2, 4, 5, 1, 2, 3, 5, 1, 2, 3, 4};
+//    Targets = {0, 1, 1, 1, 1, 0};
+//    KAO = {0,1,3,4};
+//    FO = {2,1,3,2};
+//    Targets = {0,1,0,1};
+    std::vector<double> Parray(FO.size(), p);
+    kGraph G(KAO, FO, Parray, Targets);
+    std::cout << std::setprecision(20) << ReliabilityDiamConstr(G, d);
 }
